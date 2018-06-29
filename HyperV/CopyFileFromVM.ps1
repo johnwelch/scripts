b1c5ff1b05581dcafe4cdbf822be0bba48585ef1
John Welch <john.welch@live.com>
2018-06-29 14:13:27 -0400

    HyperV Copy Files to/from host

---
+++ HyperV/CopyFileFromVM.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [string]$vmName,
+    [string]$sourceFile,
+    [string]$destinationFolder)
+
+
+$s = New-PSSession -VMName $vmName -Credential (Get-Credential)
+Copy-Item -FromSession $s -Path $sourceFile -Destination $destinationFolder
+Remove-PSSession $s
---
+++ HyperV/CopyFileToVM.ps1
@@ -0,0 +1,11 @@
+﻿param(
+    [string]$vmName,
+    [string]$sourceFile,
+    [string]$destinationFolder
+)
+
+
+
+$s = New-PSSession -VMName $vmName -Credential (Get-Credential)
+Copy-Item -ToSession $s -Path $sourceFile -Destination $destinationFolder
+Remove-PSSession $s
